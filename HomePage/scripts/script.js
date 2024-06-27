03cb58300606de899eef721938928fbf2abf227b
Jhonier Santana <jhonier_2504@hotmail.com>
2024-06-27 09:59:06 -0500

    Se termina los dos ultimos puntos de la tercera entrega

--- HomePage/scripts/script.js
+++ HomePage/scripts/script.js
@@ -144,32 +144,33 @@ const productsDetails = [
       "Discover the elegance and style of our Gold Ring with Carnelian. This ring features a central carnelian stone in a charming orange hue, cut in a square shape with gently rounded edges. The ring is adorned with a delicate beaded chain, adding a unique and sophisticated touch to its design. Perfect for any occasion, this ring flawlessly combines classic beauty with modern style. Make it yours and add a touch of distinction to your jewelry collection!",
     cantidadEnStock: {
       Rose_Gold: {
-        size_48: 10,
-        size_50: 10,
-        size_52: 10,
-        size_54: 10,
-        size_56: 10,
-        size_58: 10,
-        size_60: 10,
-        size_62: 10,
-        size_64: 10,
-        size_66: 10,
-        size_68: 10,
-        size_70: 10,
-      },
-      White: {
-        size_48: 10,
-        size_50: 10,
-        size_52: 10,
-        size_54: 10,
-        size_56: 10,
-        size_58: 10,
-        size_60: 10,
-        size_62: 10,
-        size_64: 10,
-        size_66: 10,
-        size_68: 10,
-        size_70: 10,
+        unidad: 10,
+        //   size_48: 10,
+        //   size_50: 10,
+        //   size_52: 10,
+        //   size_54: 10,
+        //   size_56: 10,
+        //   size_58: 10,
+        //   size_60: 10,
+        //   size_62: 10,
+        //   size_64: 10,
+        //   size_66: 10,
+        //   size_68: 10,
+        //   size_70: 10,
+        // },
+        // White: {
+        //   size_48: 10,
+        //   size_50: 10,
+        //   size_52: 10,
+        //   size_54: 10,
+        //   size_56: 10,
+        //   size_58: 10,
+        //   size_60: 10,
+        //   size_62: 10,
+        //   size_64: 10,
+        //   size_66: 10,
+        //   size_68: 10,
+        //   size_70: 10,
       },
     },
   },
@@ -217,7 +218,7 @@ const productsDetails = [
   },
 ];
 
-2; //filtrar elementos de un array
+// 2 filtrar elementos de un array
 function filtrarAccesorios(accesorios, tipoDeAccesorio) {
   const accesoriosFiltrados = accesorios.filter(
     (accesorios) => accesorios.tipoDeAccesorio === tipoDeAccesorio
@@ -226,6 +227,7 @@ function filtrarAccesorios(accesorios, tipoDeAccesorio) {
 }
 
 const filtroDeaccesorios = filtrarAccesorios(productsDetails, "Rings");
+console.log("Filtrar accesorios");
 console.log(filtroDeaccesorios);
 
 //3.busqueda por nombre en un array
@@ -235,5 +237,43 @@ function busquedaPorNombre(accesorios, terminoDeBusqueda) {
   );
   return busquedaAccesorios;
 }
-const terminoDeBusqueda =busquedaPorNombre(productsDetails, "Ring");
+const terminoDeBusqueda = busquedaPorNombre(productsDetails, "Glimme");
+console.log("Busqueda por nombre");
 console.log(terminoDeBusqueda);
+
+// 4. Ordenar los precios de forma ascendente y descendente
+const ordenarPorPrecio = (productos, orden) => {
+  if (orden === "ascendente") {
+    return productos.sort((a, b) => a.precioUnitario - b.precioUnitario);
+  } else if (orden === "descendente") {
+    return productos.sort((a, b) => b.precioUnitario - a.precioUnitario);
+  } else {
+    console.error('El orden debe ser "ascendente" o "descendente"');
+  }
+};
+
+console.log(
+  "Orden de precio ascendente: ",
+  ordenarPorPrecio(productsDetails, "ascendente")
+);
+
+console.log(
+  "Orden de precio descendente: ",
+  ordenarPorPrecio(productsDetails, "descendente")
+);
+
+//5. Total a pagar de una compra
+const calcularTotalCompra = (productos) => {
+  return productos.reduce((total, producto) => {
+    const unidadesTotales = Object.values(producto.cantidadEnStock).reduce(
+      (suma, stock) => {
+        return suma + stock.unidad;
+      },
+      0
+    );
+    console.log("Unidades totales:", unidadesTotales);
+    return total + unidadesTotales * producto.precioUnitario;
+  }, 0);
+};
+
+console.log("Total a pagar:", calcularTotalCompra(productsDetails));
